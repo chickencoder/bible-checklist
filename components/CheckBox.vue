641e0f0daf73501dd04a42fc08aab7568c7a502a
Jesse Sibley <inounomemynameisjesse@gmail.com>
2019-09-16 19:49:48 +0100

    Rename CheckBox component

---
+++ components/CheckBox.vue
@@ -0,0 +1,38 @@
+<template>
+  <span class="text-2xl checkbox flex items-center" :class="{
+    complete: state === 'complete',
+    pending: state === 'in-progress'
+  }">{{ label }}</span>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      default: null
+    },
+    state: {
+      type: String,
+      default: 'incomplete'
+    }
+  }
+}
+</script>
+
+<style>
+.checkbox:before {
+  content: " ";
+  @apply inline-block bg-gray-200 w-12 h-12 mr-4 rounded-full flex items-center justify-center text-white;
+}
+
+.checkbox.pending:before {
+  content: " ";
+  @apply bg-orange-200;
+}
+
+.checkbox.complete:before {
+  content: "\2713";
+  @apply bg-green-300;
+}
+</style>
